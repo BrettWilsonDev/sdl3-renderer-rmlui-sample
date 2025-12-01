57edc09edb563b0513a64e23fd65ce2e7a489cff
BrettWilsonBDW <brett.david.wilson.bdw@outlook.com>
2025-12-01 22:03:37 +0200

    Fixed msvc build

--- tools/build-sdl3-emcc.bat
+++ tools/build-sdl3-emcc.bat
@@ -4,7 +4,7 @@ pushd %~dp0
 
 mkdir ..\build-em
 
-xcopy /E /I /Y "..\assets\" "..\build-em\assets\"
+@REM xcopy /E /I /Y "..\assets\" "..\build-em\assets\"
 
 cd ..\build-em
 
@@ -12,12 +12,12 @@ IF "%1"=="async" (
     echo using asyncify
     call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=ON -DUSE_SDL2=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
 ) ELSE IF "%1"=="debug" (
-    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=ON -DUSE_SDL2=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXECUTABLE_SUFFIX=".html"
+    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=ON -DUSE_SDL2=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
 ) ELSE IF NOT "%1"=="" (
     echo Unknown argument: %1
     exit /b 1
 ) ELSE (
-    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=ON -DUSE_SDL2=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html"
+    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=ON -DUSE_SDL2=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
 )
 
 call emmake make
